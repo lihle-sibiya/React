a84efb5b7cde1e6e47219d88380cf686bac780bc
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-08-07 22:48:45 +0200

    first commit

--- Classwork/chapter2/src/App.js
+++ Classwork/chapter2/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//2.2 Using our Created Component
+import React, { Component } from 'react';
+import Products from './Products';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>My First React App!</h1>
+        <Products />
+      </div>
+    );
+  }
 }
-
 export default App;
