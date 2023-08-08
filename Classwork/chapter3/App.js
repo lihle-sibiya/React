96733c77aad3f2c0fd7b33275fd5c1a95d664873
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-08-08 23:57:43 +0200

    second commit

---
+++ Classwork/chapter3/App.js
@@ -0,0 +1,14 @@
+//2.2 Using our Created Component
+import React, { Component } from 'react';
+import Products from './Products';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>My First React App!</h1>
+        <Products />
+      </div>
+    );
+  }
+}
+export default App;
